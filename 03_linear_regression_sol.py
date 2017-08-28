4f8e9fe7e4af5a54c5985a2d5430dee35c77c3f6
yash <yashrajstha@gmail.com>
2017-08-28 23:21:51 +0200

    second

--- 03_linear_regression_sol.py
+++ 03_linear_regression_sol.py
@@ -1,12 +1,10 @@
-""" Simple linear regression example in TensorFlow
-This program tries to predict the number of thefts from 
-the number of fire in the city of Chicago
-Author: Chip Huyen
-Prepared for the class CS 20SI: "TensorFlow for Deep Learning Research"
-cs20si.stanford.edu
-"""
+ 
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -22,6 +20,10 @@ book = xlrd.open_workbook(DATA_FILE, encoding_override="utf-8")
 sheet = book.sheet_by_index(0)
 data = np.asarray([sheet.row_values(i) for i in range(1, sheet.nrows)])
 n_samples = sheet.nrows - 1
+
+
+X
+
 
 # Step 2: create placeholders for input X (number of fire) and label Y (number of theft)
 X = tf.placeholder(tf.float32, name='X')
@@ -45,8 +47,7 @@ with tf.Session() as sess:
 	# Step 7: initialize the necessary variables, in this case, w and b
 	sess.run(tf.global_variables_initializer()) 
 	
-	writer = tf.summary.FileWriter('./graphs/linear_reg', sess.graph)
-	
+ 	
 	# Step 8: train the model
 	for i in range(50): # train the model 100 epochs
 		total_loss = 0
@@ -55,9 +56,7 @@ with tf.Session() as sess:
 			_, l = sess.run([optimizer, loss], feed_dict={X: x, Y:y}) 
 			total_loss += l
 		print('Epoch {0}: {1}'.format(i, total_loss/n_samples))
-
-	# close the writer when you're done using it
-	writer.close() 
+ 
 	
 	# Step 9: output the values of w and b
 	w, b = sess.run([w, b]) 
@@ -68,3 +67,5 @@ plt.plot(X, Y, 'bo', label='Real data')
 plt.plot(X, X * w + b, 'r', label='Predicted data')
 plt.legend()
 plt.show()
+
+print(data)
